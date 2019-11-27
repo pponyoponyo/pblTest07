86f99522380f160569f999196112ea448cd9c6f0
Karen <1yw@naver.com>
2019-11-27 12:10:23 +0900

    rule set

--- app/src/main/java/com/example/pbltest07/RuleSet/EvalMethod.java
+++ app/src/main/java/com/example/pbltest07/RuleSet/EvalMethod.java
@@ -2,59 +2,28 @@ package com.example.pbltest07.RuleSet;
 
 import android.os.AsyncTask;
 
+import com.example.pbltest07.Rule;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 
-public class EvalMethod {
+public class EvalMethod extends Rule {
 
-    static private String url;
-    static private String input;
+    private String html;
 
-    static public boolean rule(final String html){
+    public EvalMethod(String url) {
+        super(url);
+        html = getHtml();
+    }
 
-        input = url;
-
-        JsoupAsyncTask jsoupHttp = new JsoupAsyncTask();
-        jsoupHttp.execute();
+    public boolean rule(){
 
         if(html.contains("eval()")){
             return true;
         }
         return false;
-    }
-
-
-    static class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
-        private String htmlData = "";
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                Document doc = Jsoup.connect(input).get();
-                htmlData = doc.html(); // html 소스 긁어오기
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-
-            if(htmlData.equals("")){
-                return;
-            }else{
-                url = htmlData;
-            }
-        }
     }
 
 }
